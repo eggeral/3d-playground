718fae5783c865d7a140f50f2dbd22eda597e191
Alexander Egger <alexander.egger@gmail.com>
2017-08-30 22:39:52 +0200

    Added simple triangle example

---
+++ src/main/kotlin/threed/main.kt
@@ -0,0 +1,41 @@
+package threed
+
+import org.khronos.webgl.WebGLRenderingContext
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.HTMLDivElement
+import kotlin.browser.document
+import kotlin.browser.window
+
+
+fun main(args: Array<String>) {
+
+    val container = document.getElementById("container") as HTMLDivElement
+    val canvas = document.createElement("canvas") as HTMLCanvasElement
+
+    val webGlContext = canvas.getContext("webgl")
+
+
+    if (webGlContext == null) {
+
+        val text = document.createTextNode("WebGl not available in this browser!")
+        container.appendChild(text)
+
+    } else {
+
+        container.appendChild(canvas)
+        webGlContext as WebGLRenderingContext
+
+        fun render() {
+
+            webGlContext.viewport(0, 0, canvas.width, canvas.height)
+            drawTriangle(webGlContext)
+            window.requestAnimationFrame { render() }
+
+        }
+        render()
+
+    }
+
+}
+
+
