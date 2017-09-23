edc765bd610205374468d4fd186678d7959b57eb
Thomas Wiesinger <wiesinger.thomas@gmail.com>
2017-09-23 10:47:05 +0200

    improved function createWebGLRenderingContext

--- src/main/kotlin/WebGL/WebGLRenderingContext.kt
+++ src/main/kotlin/WebGL/WebGLRenderingContext.kt
@@ -6,10 +6,10 @@ import org.w3c.dom.HTMLCanvasElement
 fun createWebGLRenderingContext(canvas: HTMLCanvasElement): WebGLRenderingContext {
     val webGlContext = canvas.getContext("webgl")
 
-
     if (webGlContext == null)
         console.error("WebGL not available in this browser!")
+    else
+        console.info("WebGlRenderingContext successfully created!")
 
-    console.info("WebGlRenderingContext successfully created!")
     return webGlContext as WebGLRenderingContext
 }
