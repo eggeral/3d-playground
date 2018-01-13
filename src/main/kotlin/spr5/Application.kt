996e21b656fa578a66669b59a0f8b4f3c4bee9e9
Dmitriy Purgin <dpurgin@gmail.com>
2018-01-13 14:13:12 +0100

    Scene object hierarchy: SceneObject is parent interface for WebGLDrawable, WebGLDrawaleMulticolored

---
+++ src/main/kotlin/spr5/Application.kt
@@ -0,0 +1,11 @@
+package spr5
+
+import spr5.scene.SceneObjectManager
+
+class Application {
+    val renderer: WebGLRenderer = WebGLRenderer();
+    val sceneObjectManager: SceneObjectManager = SceneObjectManager();
+
+    fun run() {
+    }
+}
