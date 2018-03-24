3889c7626ef04d6b9abdf0a3f612b24427e62aec
Peter Sswimbersky <peter.sswimbersky@mathconsult.co.at>
2018-03-24 11:21:40 +0100

    commit before end

--- src/main/kotlin/spr5/scene/SceneObjectsAttached.kt
+++ src/main/kotlin/spr5/scene/SceneObjectsAttached.kt
@@ -1,8 +1,24 @@
 package spr5.scene
 
-class SceneObjectsAttached: SceneObject
+class SceneObjectsAttached : SceneObject
 {
+    //eventuell position in der liste von SceneObjects merken.
     fun getVertices(): Array<Float>
     fun getColors(): Array<Float>
     fun getIndices(): Array<Short>
+
+    private var attachedObjects: List<SceneObject> = listOf();
+    private var coordinateNew = Coordinate(0.0f,0.0f,0.0f);
+
+    fun getAllAttachedObjects() : List<SceneObject> {
+        return attachedObjects;
+    }
+
+    fun moveAttachedObjects(attachedObjects: List<SceneObject>, oldCoordinate: Coordinate, newCoordinate: Coordinate) {
+        for (attachedObject in attachedObjects) {
+            var ao = attachedObject.getVertices();
+
+
+        }
+    }
 }
