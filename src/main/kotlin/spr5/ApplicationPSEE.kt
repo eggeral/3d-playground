f00a63f90ad8d3a913548af7b7626b76db152137
Elisabeth Egger <elisabeth.m.egger@gmail.com>
2018-03-24 14:51:51 +0100

    SceneObjectsAttached improved.

--- src/main/kotlin/spr5/ApplicationPSEE.kt
+++ src/main/kotlin/spr5/ApplicationPSEE.kt
@@ -7,9 +7,10 @@ class ApplicationPSEE {
 
     fun run() {
 
-        val center = Coordinate(0.0f, 0.0f, 0.0f);
+        val center1 = Coordinate(0.0f, 0.0f, 0.0f);
         val center2 = Coordinate(1.0f, 0.0f, 0.0f);
-        val center3 = Coordinate(4.0f, 0.0f, 0.0f);
+        val center3 = Coordinate(3.0f, 0.0f, 0.0f);
+        val center4 = Coordinate(5.0f, 0.0f, 0.0f);
         val cubeSize = 1.0f;
         val cubeFacesColors = arrayOf(Rgba(1.0f, 0.0f, 0.0f, 1.0f)
                 , Rgba(0.0f, 1.0f, 0.0f, 1.0f)
@@ -18,8 +19,13 @@ class ApplicationPSEE {
                 , Rgba(0.0f, 0.5f, 0.5f, 1.0f)
                 , Rgba(0.5f, 0.0f, 0.5f, 1.0f));
 
-        renderer.add(createMulticolorCube(center, cubeSize, cubeFacesColors));
+        renderer.add(createMulticolorCube(center1, cubeSize, cubeFacesColors));
         renderer.add(createCube(center2, 1.0f, Rgba.Red));
-        renderer.add(createCube(center3, 2.0f, Rgba.Blue));
+        renderer.add(createCube(center3, 1.0f, Rgba.Blue));
+
+
+
+        renderer.add(createSceneObjectsAttached(center4));
+
     }
 }
--- src/main/kotlin/spr5/scene/SceneObjectsAttached.kt
+++ src/main/kotlin/spr5/scene/SceneObjectsAttached.kt
@@ -1,24 +1,53 @@
 package spr5.scene
 
-class SceneObjectsAttached : SceneObject
-{
+
+class SceneObjectsAttached(var center: Coordinate) : SceneObject {
+
+
+    override fun getVertices(): Array<Float> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getColors(): Array<Float> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getIndices(): Array<Short> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
     //eventuell position in der liste von SceneObjects merken.
-    fun getVertices(): Array<Float>
-    fun getColors(): Array<Float>
-    fun getIndices(): Array<Short>
+
 
     private var attachedObjects: List<SceneObject> = listOf();
-    private var coordinateNew = Coordinate(0.0f,0.0f,0.0f);
+    private var coordinateNew = Coordinate(0.0f, 0.0f, 0.0f);
 
-    fun getAllAttachedObjects() : List<SceneObject> {
+    fun getAllAttachedObjects(): List<SceneObject> {
         return attachedObjects;
     }
 
-    fun moveAttachedObjects(attachedObjects: List<SceneObject>, oldCoordinate: Coordinate, newCoordinate: Coordinate) {
-        for (attachedObject in attachedObjects) {
-            var ao = attachedObject.getVertices();
+}
 
+fun addToAttachedObjects(listOfObjectsAttached: List<SceneObject>, objectToAttach: SceneObject): List<SceneObject> {
+    var resultList : List <SceneObject> = listOf()
+    resultList = listOfObjectsAttached
+    resultList += objectToAttach
+    return resultList
+}
 
-        }
+fun removeObjectFromAttachedObjects(listOfObjectsAttached: List<SceneObject>, objectToRemove: SceneObject): List<SceneObject> {
+    var resultList : List <SceneObject> = listOf()
+    resultList = listOfObjectsAttached
+    resultList -= objectToRemove
+    return resultList
+}
+
+fun moveAttachedObjects(attachedObjects: List<SceneObject>, oldCoordinate: Coordinate, newCoordinate: Coordinate) {
+    for (attachedObject in attachedObjects) {
+
     }
+}
+
+
+fun createSceneObjectsAttached(center: Coordinate) :SceneObject {
+    return SceneObjectsAttached(center)
 }
