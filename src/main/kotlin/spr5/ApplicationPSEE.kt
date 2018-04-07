582ee56005720fbd87d2a121fdf04d8584032a72
Elisabeth Egger <elisabeth.m.egger@gmail.com>
2018-04-07 10:25:16 +0200

    ApplicationPSEE refactored

--- src/main/kotlin/spr5/ApplicationPSEE.kt
+++ src/main/kotlin/spr5/ApplicationPSEE.kt
@@ -7,35 +7,28 @@ class ApplicationPSEE {
 
     fun run() {
 
-        val center1 = Coordinate(0.0f, 0.0f, 0.0f);
-        val center2 = Coordinate(1.0f, 0.0f, 0.0f);
-        val center3 = Coordinate(3.0f, 0.0f, 0.0f);
-        val center4 = Coordinate(5.0f, 0.0f, 0.0f);
-        val cubeSize = 1.0f;
+        val center1 = Coordinate(0.0f, 0.0f, 0.0f)
+        val center2 = Coordinate(1.0f, 0.0f, 0.0f)
+        val center3 = Coordinate(5.0f, 0.0f, 0.0f)
+        val cubeSize = 1.0f
+
         val cubeFacesColors = arrayOf(Rgba(1.0f, 0.0f, 0.0f, 1.0f)
                 , Rgba(0.0f, 1.0f, 0.0f, 1.0f)
                 , Rgba(0.0f, 0.0f, 1.0f, 1.0f)
                 , Rgba(0.5f, 0.5f, 0.0f, 1.0f)
                 , Rgba(0.0f, 0.5f, 0.5f, 1.0f)
-                , Rgba(0.5f, 0.0f, 0.5f, 1.0f));
+                , Rgba(0.5f, 0.0f, 0.5f, 1.0f))
 
 
         val cube1 = createCube(center2, 1.0f, Rgba.Red)
-        val cube2 = createCube(center4, 1.0f, Rgba.Blue)
+        val cube2 = createCube(center3, 1.0f, Rgba.Blue)
 
         val attachedCubes = SceneNodesAttached()
         attachedCubes.addChild(cube1)
         attachedCubes.addChild(cube2)
 
-        attachedCubes.position = Coordinate(-6.0f, 0.0f, 0.0f)
-        renderer.add(createMulticolorCube(center1, cubeSize, cubeFacesColors));
-//        renderer.add(createCube(center2, 1.0f, Rgba.Red));
-//        renderer.add(createCube(center3, 1.0f, Rgba.Blue));
-//
-//
-//
-//        renderer.add(createSceneObjectsAttached(center4));
+        attachedCubes.position = Coordinate(3.0f, 0.0f, 0.0f)
+        renderer.add(createMulticolorCube(center1, cubeSize, cubeFacesColors))
         renderer.add(attachedCubes)
-
     }
 }
