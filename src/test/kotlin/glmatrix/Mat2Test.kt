3207bf1250ebb686e5ac9e96436023631b3b0abe
horst <horstlijack@gmail.com>
2018-04-21 13:14:22 +0200

    adapted unit tests for glmatrix

--- src/test/kotlin/glmatrix/Mat2Test.kt
+++ src/test/kotlin/glmatrix/Mat2Test.kt
@@ -10,7 +10,7 @@ class Mat2Test {
 
     @Test
     fun create() {
-        val identity = Mat2.identity()
+        val identity = Mat2().identity()
 
         assertEquals(1.0, identity[0])
         assertEquals(0.0, identity[1])
@@ -24,72 +24,62 @@ class Mat2Test {
         val m2 = Mat2(1.0, 2.0, 3.0, 4.0)
         val m3 = Mat2(0.0, 1.0, 2.0, 3.0)
 
-        assertTrue(Mat2.equals(m1, m2))
-        assertTrue(Mat2.equals(m2, m1))
-        assertFalse(Mat2.equals(m2, m3))
-        assertFalse(Mat2.equals(m3, m2))
+        assertTrue(m1.equals(m2))
+        assertTrue(m2.equals(m1))
+        assertFalse(m2.equals(m3))
+        assertFalse(m3.equals(m2))
     }
 
     @Test
     fun testAddMatrix() {
         val m1 = Mat2(1.0, 2.0, 3.0, 4.0)
         val m2 = Mat2(1.0, 2.0, 3.0, 4.0)
-        val expected = Mat2(2.0, 4.0, 6.0, 8.0);
+        val expected = Mat2(2.0, 4.0, 6.0, 8.0)
 
-        assertTrue(Mat2.equals(expected, m1 + m2));
-        assertTrue(Mat2.equals(expected, m2 + m1));
-
-        assertTrue(Mat2.equals(expected, Mat2.add(m1, m2)));
-        assertTrue(Mat2.equals(expected, Mat2.add(m2, m1)));
-
-        assertTrue(Mat2.equals(expected, m1.add(m2)));
-        assertTrue(Mat2.equals(expected, m1));
+        assertTrue(expected.equals(m1 + m2))
+        assertTrue(expected.equals(m2 + m1))
+        assertTrue(expected.equals(m1))
     }
 
     @Test
     fun testAddZero() {
         val m1 = Mat2(1.0, 2.0, 3.0, 4.0)
         val m2 = Mat2(0.0, 0.0, 0.0, 0.0)
-        val expected = Mat2(1.0, 2.0, 3.0, 4.0);
+        val expected = Mat2(1.0, 2.0, 3.0, 4.0)
 
-        assertTrue(Mat2.equals(expected, m1 + m2));
-        assertTrue(Mat2.equals(expected, m2 + m1));
-
-        assertTrue(Mat2.equals(expected, Mat2.add(m1, m2)));
-        assertTrue(Mat2.equals(expected, Mat2.add(m2, m1)));
-
-        assertTrue(Mat2.equals(expected, m1.add(m2)));
-        assertTrue(Mat2.equals(expected, m1));
+        assertTrue(expected.equals(m1 + m2))
+        assertTrue(expected.equals(m2 + m1))
+        assertTrue(expected.equals(m1))
     }
 
     @Test
     fun multiplyWithIdentity() {
-        val identity = Mat2.identityDoubleArray()
-        val m = Mat2.fromValues(1.0, 2.0, 3.0, 4.0)
+        val identity = Mat2().identity()
+        val m = Mat2(1.0, 2.0, 3.0, 4.0)
 
         // test m * I
-        assertTrue(Mat2.equals(m, Mat2.multiply(m, identity)))
+        assertTrue(m.equals(m * identity))
         // test I * m
-        assertTrue(Mat2.equals(m, Mat2.multiply(identity, m)))
+        assertTrue(m.equals(identity * m))
     }
 
     @Test
     fun multiplyWithMatrix() {
-        val m1 = Mat2.fromValues(1.0, 2.0, 3.0, 4.0)
-        val m2 = Mat2.fromValues(3.0, 0.0, 1.0, 2.0)
+        val m1 = Mat2(1.0, 2.0, 3.0, 4.0)
+        val m2 = Mat2(3.0, 0.0, 1.0, 2.0)
 
-        val prod1 = Mat2.multiply(m2, m1)  // computes m1 * m2
-        val prod2 = Mat2.multiply(m1, m2)  // computes m2 * m1
+        val prod1 = m2 * m1
+        val prod2 = m1 * m2
 
-        assertTrue(Mat2.equals(prod1, Mat2.fromValues(5.0, 4.0, 13.0, 8.0)))
-        assertTrue(Mat2.equals(prod2, Mat2.fromValues(3.0, 6.0, 7.0, 10.0)))
+        assertTrue(prod1.equals(Mat2(5.0, 4.0, 13.0, 8.0)))
+        assertTrue(prod2.equals(Mat2(3.0, 6.0, 7.0, 10.0)))
 
         val m11 = Mat2(1.0, 2.0, 3.0, 4.0)
         val m22 = Mat2(3.0, 0.0, 1.0, 2.0)
 
         val prod11 = m11 * m22
         val prod22 = m22 * m11
-        assertTrue(Mat2.equals(prod11, Mat2(5.0, 4.0, 13.0, 8.0)))
-        assertTrue(Mat2.equals(prod22, Mat2(3.0, 6.0, 7.0, 10.0)))
+        assertTrue(prod11.equals(Mat2(5.0, 4.0, 13.0, 8.0)))
+        assertTrue(prod22.equals(Mat2(3.0, 6.0, 7.0, 10.0)))
     }
 }
--- src/test/kotlin/glmatrix/Mat2dTest.kt
+++ src/test/kotlin/glmatrix/Mat2dTest.kt
@@ -9,7 +9,7 @@ class Mat2dTest {
 
     @Test
     fun create() {
-        val identity = Mat2d.identityDoubleArray()
+        val identity = Mat2d().identity()
 
         assertEquals(1.0, identity[0])
         assertEquals(0.0, identity[1])
@@ -23,67 +23,57 @@ class Mat2dTest {
 
     @Test
     fun testEquals() {
-        val m1 = Mat2d.fromValues(1.0, 2.0, 3.0, 4.0, 1.0, 1.0)
-        val m2 = Mat2d.fromValues(1.0, 2.0, 3.0, 4.0, 1.0, 1.0)
-        val m3 = Mat2d.fromValues(0.0, 1.0, 2.0, 3.0, 0.0, -1.0)
+        val m1 = Mat2d(1.0, 2.0, 3.0, 4.0, 1.0, 1.0)
+        val m2 = Mat2d(1.0, 2.0, 3.0, 4.0, 1.0, 1.0)
+        val m3 = Mat2d(0.0, 1.0, 2.0, 3.0, 0.0, -1.0)
 
-        assertTrue(Mat2d.equals(m1, m2))
-        assertTrue(Mat2d.equals(m2, m1))
-        assertFalse(Mat2d.equals(m2, m3))
-        assertFalse(Mat2d.equals(m3, m2))
+        assertTrue(m1.equals(m2))
+        assertTrue(m2.equals(m1))
+        assertFalse(m2.equals(m3))
+        assertFalse(m3.equals(m2))
     }
 
     @Test
     fun testAddMatrix() {
         val m1 = Mat2d(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
         val m2 = Mat2d(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
-        val expected = Mat2d(2.0, 4.0, 6.0, 8.0, 10.0, 12.0);
+        val expected = Mat2d(2.0, 4.0, 6.0, 8.0, 10.0, 12.0)
 
-        assertTrue(Mat2d.equals(expected, m1 + m2));
-        assertTrue(Mat2d.equals(expected, m2 + m1));
-
-        assertTrue(Mat2d.equals(expected, Mat2d.add(m1, m2)));
-        assertTrue(Mat2d.equals(expected, Mat2d.add(m2, m1)));
-
-        assertTrue(Mat2d.equals(expected, m1.add(m2)));
-        assertTrue(Mat2d.equals(expected, m1));
+        assertTrue(expected.equals(m1 + m2))
+        assertTrue(expected.equals(m2 + m1))
+        assertTrue(expected.equals(m1))
     }
 
     @Test
     fun testAddZero() {
         val m1 = Mat2d(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
         val m2 = Mat2d(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
-        val expected = Mat2d(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+        val expected = Mat2d(1.0, 2.0, 3.0, 4.0, 5.0, 6.0)
 
-        assertTrue(Mat2d.equals(expected, m1 + m2));
-        assertTrue(Mat2d.equals(expected, m2 + m1));
-
-        assertTrue(Mat2d.equals(expected, Mat2d.add(m1, m2)));
-        assertTrue(Mat2d.equals(expected, Mat2d.add(m2, m1)));
-
-        assertTrue(Mat2d.equals(expected, m1.add(m2)));
-        assertTrue(Mat2d.equals(expected, m1));
+        assertTrue(expected.equals(m1 + m2))
+        assertTrue(expected.equals(m2 + m1))
+        assertTrue(expected.equals(m1))
     }
 
     @Test
     fun multiplyWithIdentity() {
-        val identity = Mat2d.identityDoubleArray()
-        val m = Mat2d.fromValues(1.0, 2.0, 3.0, 4.0, 1.0, 1.0)
+        val identity = Mat2d().identity()
+        val m = Mat2d(1.0, 2.0, 3.0, 4.0, 1.0, 1.0)
 
         // test m * I
-        assertTrue(Mat2d.equals(m, Mat2d.multiply(m, identity)))
+        assertTrue(m.equals(m * identity))
         // test I * m
-        assertTrue(Mat2d.equals(m, Mat2d.multiply(identity, m)))
+        assertTrue(m.equals(identity * m))
     }
 
     @Test
     fun multiplyWithMatrix() {
-        val m1 = Mat2d.fromValues(1.0, 2.0, 3.0, 4.0, 1.0, 1.0)
-        val m2 = Mat2d.fromValues(3.0, 0.0, 1.0, 2.0, 4.0, -1.0)
-        val prod1 = Mat2d.multiply(m2, m1)  // computes m1 * m2
-        val prod2 = Mat2d.multiply(m1, m2)  // computes m2 * m1
+        val m1 = Mat2d(1.0, 2.0, 3.0, 4.0, 1.0, 1.0)
+        val m2 = Mat2d(3.0, 0.0, 1.0, 2.0, 4.0, -1.0)
+        val prod1 = m2 * m1
+        val prod2 = m1 * m2
 
-        assertTrue(Mat2d.equals(prod1, Mat2d.fromValues(5.0, 4.0, 13.0, 8.0, 8.0, 1.0)))
-        assertTrue(Mat2d.equals(prod2, Mat2d.fromValues(3.0, 6.0, 7.0, 10.0, 2.0, 5.0)))
+        assertTrue(prod1.equals(Mat2d(5.0, 4.0, 13.0, 8.0, 8.0, 1.0)))
+        assertTrue(prod2.equals(Mat2d(3.0, 6.0, 7.0, 10.0, 2.0, 5.0)))
     }
 }
